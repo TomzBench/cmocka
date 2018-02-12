0babadf6f625d4d4a88c9575a3ce55830339877e
thomas <thomas.chiantia@gmail.com>
2018-02-12 18:18:08 -0500

    turn off examples

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,6 +1,7 @@
 option(WITH_STATIC_LIB "Build with a static library" OFF)
 option(WITH_CMOCKERY_SUPPORT "Install a cmockery header" OFF)
 option(UNIT_TESTING "Build with unit testing" OFF)
+option(CMOCKA_EXAMPLES "Build with examples" OFF)
 
 if (UNIT_TESTING)
     set(WITH_STATIC_LIB ON)
